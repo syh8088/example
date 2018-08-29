5fbf652d3134172386c65ba94b8c4b0cd2f9ee7b
syh8088 <syh8088@gmail.com>
2018-08-30 00:28:25 +0900

    test
    test

--- src/test/java/com/example/api/ExampleApplicationTests.java
+++ src/test/java/com/example/api/ExampleApplicationTests.java
@@ -20,24 +20,18 @@ public class ExampleApplicationTests {
 
     @Test
     public void enumTest() {
-        System.out.println(AppNotice.Category.EVENT.name());
-        System.out.println(AppNotice.Category.valueOf("EVENT"));
+        //System.out.println(AppNotice.Category.EVENT.name());
+        //System.out.println(AppNotice.Category.valueOf("EVENT"));
 
         List<Integer> list = Arrays.asList(1, 2, 3);
         List<String> noticeName = Arrays.asList("mobile_web", "sport_android", "sport_ios", "game_android", "game_ios");
 
         noticeName.forEach(e -> {
-            System.out.println(e);
-
-
-
+            //System.out.println(e);
         });
 
+
         IntStream.range(0, 5).forEach(i -> {
-
-
-
-
         });
 
         list.forEach(i -> {
@@ -50,6 +44,11 @@ public class ExampleApplicationTests {
         });
 
 
+
+        List<Integer> list1 = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        list1.stream().forEach((i) -> {
+            System.out.println(i);
+        });
 
 
     }
