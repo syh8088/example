9173255ffe48129459f0ff1dbb5004b66c576876
syh80881 <syh8088@gmail.com>
2018-11-17 22:06:21 +0900

    Interceptor

---
+++ src/main/java/com/example/api/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.example.api.config;
+
+import com.example.api.config.interceptor.RequestInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    private RequestInterceptor requestInterceptor;
+
+    @Autowired
+    public WebConfig(RequestInterceptor requestInterceptor) {
+        this.requestInterceptor = requestInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(requestInterceptor)
+                .addPathPatterns("/*")
+                .excludePathPatterns("/test/**/")
+                .excludePathPatterns("/login"); //로그인 쪽은 예외처리를 한다.
+    }
+}
