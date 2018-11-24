63e151564422798fd4ae19e9ab5cb41728b6e1c9
syh80881 <syh8088@gmail.com>
2018-11-24 10:56:24 +0900

    WebConfig 정리
    WebConfig 정리

--- src/main/java/com/example/api/config/WebConfig.java
+++ src/main/java/com/example/api/config/WebConfig.java
@@ -2,9 +2,15 @@ package com.example.api.config;
 
 import com.example.api.config.interceptor.RequestInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
 
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
@@ -16,8 +22,23 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         this.requestInterceptor = requestInterceptor;
     }
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver resolver = new CookieLocaleResolver();
+        resolver.setCookieName("lang");
+        resolver.setDefaultLocale(Locale.KOREA);
+        return resolver;
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+
+        // Locale
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeChangeInterceptor);
+
+        // Interceptor
         registry.addInterceptor(requestInterceptor)
                 .addPathPatterns("/*")
                 .excludePathPatterns("/test/**/")
