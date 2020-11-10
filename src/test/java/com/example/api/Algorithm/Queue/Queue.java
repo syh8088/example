4ef03ce2d6532a0a977327d55af240bf6bcd9ffc
syh8088 <syh8088@gmail.com>
2020-11-10 19:18:23 +0900

    Create Queue.java
    queue 알고리즘

---
+++ src/test/java/com/example/api/Algorithm/Queue/Queue.java
@@ -0,0 +1,82 @@
+package com.example.api.Algorithm.Queue;
+
+import org.apache.ibatis.type.Alias;
+
+import java.util.NoSuchElementException;
+
+@Alias("AlgorithmQueue")
+public class Queue<T> {
+
+    class Node<T> {
+        private T data;
+        private Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+    private Node<T> first;
+    private Node<T> last;
+
+    public void add(T item) {
+
+        Node<T> t = new Node<>(item);
+
+        if (last != null) {
+            last.next = t;
+        }
+
+        last = t;
+
+        if (first == null) {
+            first = last;
+        }
+    }
+
+    public T remove() {
+
+        if (first == null) {
+            throw new NoSuchElementException();
+        }
+
+        T data = first.data;
+        first = first.next;
+
+        if (first == null) {
+            last = null;
+        }
+
+        return data;
+    }
+
+    public T peek() {
+
+        if (first == null) {
+            throw new NoSuchElementException();
+        }
+
+        return first.data;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+
+    public static void main(String[] args) {
+        Queue<Integer> q = new Queue<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        System.out.println("q.remove() = " + q.remove());
+        System.out.println("q.remove() = " + q.remove());
+        System.out.println("q.peek() = " + q.peek());
+        System.out.println("q.remove() = " + q.remove());
+        System.out.println("q.isEmpty() = " + q.isEmpty());
+        System.out.println("q.remove() = " + q.remove());
+        System.out.println("q.isEmpty() = " + q.isEmpty());
+    }
+
+}
